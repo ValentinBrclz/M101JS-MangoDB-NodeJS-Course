da5b5bf2c3d3e4ad103568cb0a7c424f1c6a3bbc
Valentin Brclz <valentin@brclz.ch>
2015-10-15 01:43:11 +0300

    Course 1 Part 02 - Then express hello world

---
+++ 1-02-HelloWorld_Drivers/3-express-swig.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Valentin Brclz
+ * Github Profile: https://github.com/ValentinBrclz
+ * Date: 14/10/2015
+ * Project: M101JS-MongoDB-NodeJS-Course
+ * Licence: See LICENCE in root directory
+ */
+var express = require('express'),
+    app = express(),
+    port = 8080,
+    cons = require('consolidate');
+
+// Handle only the root
+app.get('/', function (req, res) {
+    res.send("Hello World !");
+});
+
+// Handle all other routes -> 404
+app.get('*', function (req, res) {
+    res.send("Page not found", 404);
+});
+
+// Listening
+app.listen(port);
+console.log("Express server started on port " + port)
